08f1999ce2f6a8763504b7178f6421a874be5e4f
Chalanika <chalanikasewwandi@gmail.com>
2020-06-29 12:17:06 +0530

    created rotues

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router,Route} from 'react-router-dom'; 
+import ExerciseList from './component/exercises-list';
+import NavBar from './component/navbar';
+import EditExercise from './component/edit-exercise';
+import CreateExercise from './component/create-exercise';
+import CreateUser from './component/create-user';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar/>
+      <br/>
+      <Route path='/' component={ExerciseList}></Route>
+      <Route path='/edit/:id' component={EditExercise}></Route>
+      <Route path='/create' component={CreateExercise}></Route>
+      <Route path='/user' component={CreateUser}></Route>
+    </Router>
+    
   );
 }
 
