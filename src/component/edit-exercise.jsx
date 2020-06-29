76fd8c80f9a02459da7e1d40cb914f1fafd4b201
Chalanika <chalanikasewwandi@gmail.com>
2020-06-29 12:16:41 +0530

    created component files

---
+++ src/component/edit-exercise.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class EditExercise extends Component {
+    state = {  }
+    render() { 
+        return ( 
+        <div></div> );
+    }
+}
+ 
+export default EditExercise;
