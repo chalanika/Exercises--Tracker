340bf5d39c61b00cf11c095fb2f992caefe4ef76
Chalanika <chalanikasewwandi@gmail.com>
2020-06-29 15:33:14 +0530

    created exercise log form

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -5,18 +5,18 @@ class NavBar extends Component {
     state = {}
     render() {
         return (
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <Link to="/" className=" navbar-brand">ExerciseTracker</Link>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item active">
-                            <Link class="nav-link" to='/'>Exercises</Link>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item active">
+                            <Link className="nav-link" to='/'>Exercises</Link>
                         </li>
-                        <li class="nav-item">
-                            <Link class="nav-link" to='/create'>Create Exercise Log</Link>
+                        <li className="nav-item">
+                            <Link className="nav-link" to='/create'>Create Exercise Log</Link>
                         </li>
-                        <li class="nav-item">
-                            <Link class="nav-link" to='/user'>Create User</Link>
+                        <li className="nav-item">
+                            <Link className="nav-link" to='/user'>Create User</Link>
                         </li>
                     </ul>
                 </div>
