ed3360fa92dc6af3da347006640c3108f5dc869d
Chalanika <chalanikasewwandi@gmail.com>
2020-06-30 00:54:11 +0530

    updated exercises.js

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 let Exercise = require('../models/exercise.model');
 
-router.route('./').get(async (req,res)=>{ 
+router.get('/', async (req,res)=>{ 
     try {
         exercises = await Exercise.find();
         res.json(exercises);
@@ -10,7 +10,7 @@ router.route('./').get(async (req,res)=>{
     }
 });
 
-router.route('/add').post(async (req,res)=>{
+router.post('/add' , async (req,res)=>{
     console.log(req.body);
     const newExercise = new Exercise({
         username : req.body.username,
@@ -21,10 +21,54 @@ router.route('/add').post(async (req,res)=>{
     try {
         const saved = await newExercise.save();
         console.log(saved);
-        res.json(saved);
+        res.json('saved');
     } catch (error) {
         res.json({ message: error });
     }
 });
+
+router.get('/:id',async (req,res)=>{  
+    try {
+        const result = await Exercise.findById(req.params.id);
+        res.json(result);
+    } catch (error) {
+        res.json({ message: error });
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    console.log('delete',req.params.lawyerId);
+    try {
+        const res = await Exercise.findByIdAndDelete(req.params.id);
+        res.json('Exercise delete');
+    } catch (error) {
+        res.json({ message: error });
+    }
+});
+
+router.get('/:id' , async (req,res)=>{  
+    try {
+        const res = await Exercise.findById(req.params.id);
+        res.json(res);
+    } catch (error) {
+        res.json({ message: error });
+    }
+});
+
+router.put('/:id' , async (req,res)=>{
+    try {
+        const updateUser = await Exercise.findById({ _id: req.params.id });
+        updateUser.username = req.body.username;
+        updateUser.description = req.body.description;
+        updateUser.duration = req.body.duration;
+        updateUser.date = req.body.date;
+        
+        const saved = await Exercise.findByIdAndUpdate(req.params.id, updateUser, { new: true });
+        res.json(saved);
+
+    } catch (error) {
+        res.json({ message: error });
+    }
+})
 
 module.exports = router;
